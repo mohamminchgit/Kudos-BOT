70152d1af24b01e1eed8c8a0b9cca1452d1c11ac
mohamminchgit <mohamminch@gmail.com>
2025-05-31 17:53:49 +0330

    حذف فایل‌های غیرضروری شامل bot_fixed.py، bot_runner.log، check_db.py، fix_db.py و run_bot.py. این تغییرات به بهبود ساختار پروژه و کاهش حجم فایل‌ها کمک می‌کند.

--- bot_fixed.py
+++
@@ -1,9 +0,0 @@
-﻿The regular expression pattern season_name = c\.fetchone\(\)\[0\] if c\.fetchone\(\) else " ??????\ is not 
-valid.
-At line:1 char:1
-+ (Get-Content bot.py) -replace 'season_name = c\.fetchone\(\)\[0\] if  ...
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : InvalidOperation: (season_name = c... else " ??????\:String) [], RuntimeExce 
-   ption
-    + FullyQualifiedErrorId : InvalidRegularExpression
- 
